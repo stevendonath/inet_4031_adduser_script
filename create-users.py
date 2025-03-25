c9c08c1ef1beaca4a88da775981fac2ad9dadfc5
stevendonath <stevenadjr@gmail.com>
2025-03-25 20:40:05 +0000

    third commit

--- create-users.py
+++ create-users.py
@@ -1,71 +1,61 @@
 #!/usr/bin/python3
 
 # INET4031
-# Your Name
-# Data Created
-# Date Last Modified
+# Steven Donath
+# Date Created: 25 March 2025
+# Date Last Modified: 25 March 2025
 
-#REPLACE THIS COMMENT - identify what each of these imports is for.
+#os: Provides functions to run system commands and interact directly with the OS
+#re: Provides regular expressions for pattern matching
+#sys: Provides access to system parameters/functions like stdin for reading input
 import os
 import re
 import sys
 
-#YOUR CODE SHOULD HAVE NONE OF THE INSTRUCTORS COMMENTS REMAINING WHEN YOU ARE FINISHED
-#PLEASE REPLACE INSTRUCTOR "PROMPTS" WITH COMMENTS OF YOUR OWN
-
 def main():
     for line in sys.stdin:
 
-        #REPLACE THIS COMMENT - this "regular expression" is searching for the presence of a character - what is it and why?
-        #The important part is WHY it is looking for a particular characer - what is that character being used for?
+        #This regular expression searches for lines that start with a '#' (comment lines)
         match = re.match("^#",line)
 
-        #REPLACE THIS COMMENT - why is the code doing this?
+        #This code removes whitespace and replaces it with a ':' to create a list where each element corresponds to a field
         fields = line.strip().split(':')
 
-        #REPLACE THESE COMMENTS with a single comment describing the logic of the IF 
-        #what would an appropriate comment be for describing what this IF statement is checking for?
-        #what happens if the IF statement evaluates to true?
-        #how does this IF statement rely on what happened in the prior two lines of code? The match and fields lines.
-        #the code clearly shows that the variables match and the length of fields is being checked for being != 5  so why is it doing that?
+        #The if statement checks if the line is a comment (starts with '#') or if the number of fields does not equal 5. If so, it skips the line.
         if match or len(fields) != 5:
             continue
 
-        #REPLACE THIS COMMENT - what is the purpose of the next three lines. How does it relate to what is stored in the passwd file?
+        # This code extracts the username, password, and full name from thee fields list
         username = fields[0]
         password = fields[1]
         gecos = "%s %s,,," % (fields[3],fields[2])
 
-        #REPLACE THIS COMMENT - why is this split being done?
+        # Splits the code by commas to support multiple groups.
         groups = fields[4].split(',')
 
-        #REPLACE THIS COMMENT - what is the point of this print statement?
+        #Prints message to confirm account creation for the username
         print("==> Creating account for %s..." % (username))
-        #REPLACE THIS COMMENT - what is this line doing?  What will the variable "cmd" contain.
+        #Command to add user with the provided detail (without setting a password)
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
 
-        #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
-        #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
         #print cmd
-        #os.system(cmd)
+        os.system(cmd)
 
-        #REPLACE THIS COMMENT - what is the point of this print statement?
+        #Prints message to confiirm the password is being set for the user
         print("==> Setting the password for %s..." % (username))
-        #REPLACE THIS COMMENT - what is this line doing?  What will the variable "cmd" contain. You'll need to lookup what these linux commands do.
+        #Command to set the user's password using echo to input the password
         cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
 
-        #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
-        #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
         #print cmd
-        #os.system(cmd)
+        os.system(cmd)
 
         for group in groups:
-            #REPLACE THIS COMMENT with one that answers "What is this IF statement looking for and why? If group !='-' what happens?"
+            #If statement to check if the group is a placeholder with '-' character. If the group is valid, assigns the user to the group.
             if group != '-':
                 print("==> Assigning %s to the %s group..." % (username,group))
-                cmd = "/usr/sbin/adduser %s %s" % (username,group)
+                cmd = "/usr/sbin/adduser %s %s" % (username,group) # Command to add the user to the group
                 #print cmd
-                #os.system(cmd)
+                os.system(cmd)
 
 if __name__ == '__main__':
     main()
---
+++ readme.md
@@ -0,0 +1,44 @@
+# INET 4031 Add Users Script and User List 
+
+## Program Description
+
+The Add Users Script automates the process of creating new users on an Ubuntu system. Typically a sysadmin must manually execute a series of commands to add a user, such as 'adduser', 'passwd', and 'adduser <username> <group>' for each individual user. These commands require specific information for the user's account like username, password, and full name, but automates the process by reading this information from an input file and executing the sequence of commands. This allows the sysadmin to quickly create/configure multiple users with minimal effort.
+
+
+The script reads an input file containing the relevant user info and then processes each line before executing the sequence of commands to: 
+1. Create the user account using 'adduser'
+2. Set the user password using 'passwd'
+3. Assign the user to specific groups
+
+## Program User Operation
+
+To run the program, provide the input file in the specific format, including details such as username, password, full name, and groups. Then execute the script on the command line using ./create-users.py < create-users.input
+Before running the script, ensure that the Python script is executable by running chmod +x create-users.py
+
+## Input File Focreate
+
+These fields include:
+
+1. **Username**: username for new user account
+2. **Password**: password for new user account
+3. **Last Name**: last name of the user 
+4. **First Name**: first name of the user 
+5. **Groups**: separated by commas, a list of groups the user should be added to
+
+Example input file:
+
+```user01:pass01:Last01:First01:group01
+user02:pass02:Last02:First02:group01,group02
+user03:pass03:Last03:First03:-```
+
+To skip a line, prefix the line with # to designate it as a comment. The script will ignore any commented lines or lines that do not contain exactly 5 fields.
+If you do not wish to add a new user to any groups, ensure the fifth field contains the '-' character.
+
+
+## Command Execution 
+
+Before running the script, ensure that the Python script is executable by running chmod +x create-uers.py prior to running ./create-users.py < create-users.input to execute the code.
+
+## Dry Run
+
+To run the script without having an effect on the system and to test the code, ensure that all os.system(cmd) lines are COMMENTED OUT. Otherwise, these commands will be ran by the system and executed. This allows for testing and ensuring everything is set up correctly prior to making important changes. 
